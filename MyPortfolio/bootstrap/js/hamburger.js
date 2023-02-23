65c6c016e4d0bfab22a8227a7ba55997a16adcd4
jamesrusso12 <124114717+jamesrusso12@users.noreply.github.com>
2023-02-22 18:47:50 -0700

    Add files via upload

---
+++ MyPortfolio/bootstrap/js/hamburger.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
